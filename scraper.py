07935ddd9818830deb790fd6c6d493fc609783cb
Alex Macharia <alex.machariam@gmail.com>
2020-04-26 17:57:07 +0300

    Added kafka producer

--- scraper.py
+++ scraper.py
@@ -1,8 +1,9 @@
-import requests
-from bs4 import BeautifulSoup
-import json
 from time import sleep
+from json import dumps
+import requests
 import numpy as np
+from bs4 import BeautifulSoup
+from kafka import KafkaProducer
 
 
 def fetch_url(url, headers):
@@ -11,12 +12,8 @@ def fetch_url(url, headers):
         if response.status_code == 200:
             html = response.text
             return html
-    except Exception as e:
-        print(str(e))
-
-
-def fetch_recipe():
-    pass
+    except Exception as ex:
+        print(str(ex))
 
 
 def sleep_random():
@@ -24,7 +21,7 @@ def sleep_random():
     sleep(np.random.choice(options))
 
 
-def parse_url(html, headers):
+def parse_url(html, headers, producer_instance):
     soup = BeautifulSoup(html, 'lxml')
     url_list = soup.findAll('a', attrs={'class':
                                         'fixed-recipe-card__title-link'})
@@ -33,14 +30,38 @@ def parse_url(html, headers):
         recipe_title = item.find('span').text
         print('{}     {}'.format(recipe_title, recipe_url))
         recipe_html = fetch_url(recipe_url, headers)
+        data = {recipe_title: recipe_html}
+        publish_message(producer_instance, data)
         sleep_random()
+
+
+def connect_kafka_producer():
+    try:
+        _producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
+                                  value_serializer=lambda x:
+                                  dumps(x).encode('utf-8'))
+    except Exception as ex:
+        print(str(ex))
+    finally:
+        return _producer
+
+
+def publish_message(producer_instance, data):
+    try:
+        producer_instance.send('allrecipe_data', value=data)
+        producer_instance.flush()
+        print('Message published successfully')
+    except Exception as ex:
+        print(str(ex))
 
 
 if __name__ == '__main__':
     url = "https://www.allrecipes.com/recipes/88/bbq-grilling/"
-    headers = {'user-agent': '''Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6)
-        AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87
-               Safari/537.36''', 'Pragma': 'no-cache'}
-
+    headers = {'User-Agent': '''Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6)
+               AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari
+               537.36''',
+               'Pragma': 'no-cache'
+               }
     html = fetch_url(url, headers)
-    parse_url(html, headers)
+    producer_instance = connect_kafka_producer()
+    parse_url(html, headers, producer_instance)
